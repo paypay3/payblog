084f31b591720523b2686c10b01c8a9d284572d8
Yusuke Taira <ney07214545@icloud.com>
2020-04-01 17:01:54 +0900

    #62 Modify config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    import debug_toolbar
+# if settings.DEBUG:
+#     import debug_toolbar
 
-    urlpatterns += [
-        path('__debug__/', include(debug_toolbar.urls)),
-    ]
+#     urlpatterns += [
+#         path('__debug__/', include(debug_toolbar.urls)),
+#     ]
